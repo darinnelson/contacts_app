673f23739d4a67353b4d1535ba6bd9fda5681701
darinnelson <dnelson5614@gmail.com>
2018-10-24 19:24:45 -0500

    middle name

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -8,6 +8,7 @@ class Api::ContactsController < ApplicationController
   def create
     @contact = Contact.new(
       first_name: params["first_name"],
+      middle_name: params["middle_name"],
       last_name: params["last_name"],
       email: params["email"]
     )
@@ -24,6 +25,7 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find_by(id: params[:id])
     @contact.id = params[:id] || @contact.id
     @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_225329) do
+ActiveRecord::Schema.define(version: 2018_10_25_000335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_10_23_225329) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "middle_name"
   end
 
 end
